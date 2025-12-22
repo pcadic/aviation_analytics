4f439baee56c192d5a878a255f57b7215336d726
Philippe Cadic <87199501+pcadic@users.noreply.github.com>
2025-12-22 14:17:56 -0800

    Update weather.py

--- open-meteo/weather.py
+++ open-meteo/weather.py
@@ -127,7 +127,7 @@ def main():
     # 2️⃣ Airports (lat / lon)
     # --------------------------------------------------------------
 
-    airports = supabase.table("airports_referencess") \
+    airports = supabase.table("airports_reference") \
         .select("icao, latitude, longitude") \
         .execute().data
 
