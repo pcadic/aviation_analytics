ecb9e52a5d8aa75bbbc8740bbcb3df98189ce084
Philippe Cadic <87199501+pcadic@users.noreply.github.com>
2025-12-21 21:44:21 -0800

    Update weather.py

--- open-meteo/weather.py
+++ open-meteo/weather.py
@@ -4,7 +4,7 @@ from dateutil import parser
 from supabase import create_client
 
 SUPABASE_URL = os.environ["SUPABASE_URL"]
-SUPABASE_KEY = os.environ["SUPABASE_SERVICE_KEY"]
+SUPABASE_KEY = os.environ["SUPABASE_SERVICE_ROLE_KEY"]
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
