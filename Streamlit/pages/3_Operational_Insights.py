1414f2f2bc5cc2ce2b8c50a77751e179c1dc0052
Philippe Cadic <87199501+pcadic@users.noreply.github.com>
2025-12-30 13:43:57 -0800

    Update 3_Operational_Insights.py

--- Streamlit/pages/3_Operational_Insights.py
+++ Streamlit/pages/3_Operational_Insights.py
@@ -13,8 +13,8 @@ st.set_page_config(
     layout="wide"
 )
 
-SUPABASE_URL = os.environ["SUPABASE_URL"]
-SUPABASE_KEY = os.environ["SUPABASE_SERVICE_ROLE_KEY"]
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_ANON_KEY"]
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
