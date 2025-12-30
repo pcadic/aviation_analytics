6cbfac6342fc79d556448d747cc64a8cedc6a032
Philippe Cadic <87199501+pcadic@users.noreply.github.com>
2025-12-30 13:43:43 -0800

    Update 2_Overview.py

--- Streamlit/pages/2_Overview.py
+++ Streamlit/pages/2_Overview.py
@@ -7,8 +7,8 @@ from supabase import create_client
 import streamlit as st
 
 
-SUPABASE_URL = os.environ["SUPABASE_URL"]
-SUPABASE_KEY = os.environ["SUPABASE_SERVICE_ROLE_KEY"]
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_ANON_KEY"]
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
